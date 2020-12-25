93a96418fac774ffc261fc5497208748149585e7
ryujihagiwara <dragon7103next@icloud.com>
2020-12-25 22:22:37 +0900

    booksコントローラー追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
