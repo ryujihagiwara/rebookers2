46798c674d57abf9ec0f211c8a5cde8cdd429437
ryujihagiwara <dragon7103next@icloud.com>
2021-01-13 21:48:06 +0900

    Users indexビュー終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image
+
+  validates :name, presence: true
+  validates :name, length: { in: 2..20 }
+
+  validates :introduction, length: { maximum: 50 }
+
+
+
 end
