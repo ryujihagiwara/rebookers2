be7746d7ff5717e09ded29a4337ce9b7d2a55db7
ryujihagiwara <dragon7103next@icloud.com>
2021-01-13 19:12:44 +0900

    Books 部分終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-    @user = current_user
   end
 
   def create
   	@book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def show
@@ -25,13 +28,16 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-       redirect_to book_path(@book)
+       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
